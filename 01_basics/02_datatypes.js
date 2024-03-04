131377ed1d36475d7a783445c7aa8c938a4bf7ac
Shivam <kushwahashivam050@gmail.com>
2024-03-04 14:34:38 +0530

    data types done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+// ---------- DATA TYPES IN JS ------------ 
+
+// <====  Primitive data types ====> 
+// number 
+// bigint 
+// string => "", '', ``
+// boolean => true / false 
+// undefined 
+// null
+// symbol => unique 
+
+// <===  Non-Primitive data types ===> 
+// Object 
+// Array
+
+// <===  Special for Interview ===> 
+
+let obj = {
+    name: "Raj",
+    role: "Devloper",
+    age: 25
+}
+
+let arr = [1, 2, 3, 4, 5]
+console.log(typeof (undefined)) // type of undefined is *undefined* 
+console.log(typeof (null)) // type of null is *object* 
+console.log(typeof (obj)) //  type of object is *object* 
+console.log(typeof (arr)) //  type of array is *object* 
