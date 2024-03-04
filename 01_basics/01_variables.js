26d6daadede357c8cf0a1f63c57ad217ae9a3266
Shivam <kushwahashivam050@gmail.com>
2024-03-04 14:12:47 +0530

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,34 @@
+// var - used for making the variables 
+// nowadays deprecating the var 
+var number = 10;
+console.log("Before changing the value of number is : " + number);
+// we can change the value of the varible 
+number = 15;
+console.log("After changing the value of number is : " + number);
+
+// let - used for making the variables 
+let city = "jaipur";
+console.log("Before changing the value of city is : " + city);
+city = "delhi";
+console.log("After changing the value of city is : " + city);
+
+// const - used for making the constants 
+const accountId = 100;
+console.log("The account id is : " + accountId);
+// accountId = 150; --> this will throw error because we can not change the value of constant declared once 
+
+let studentName;    // this is undefined variable because it's value not defined now 
+console.log(studentName);
+
+bankName = "bank of baroda"; // javascript internally can recognize the type of varible and it behaves like global variable 
+console.log(bankName);
+
+
+
+
+
+
+
+
+// const -- for making the constants 
+const marks = 99;
