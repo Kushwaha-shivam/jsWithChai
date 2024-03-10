bce46964b3499170ed089f08a561aba6a2402c53
Shivam <kushwahashivam050@gmail.com>
2024-03-10 15:08:26 +0530

    while do while done

---
+++ 05_loops.js/02_while_do_while.js
@@ -0,0 +1,32 @@
+// while loop 
+// +++++++++++ Syntax +++++++++ 
+// while (condition) {
+//     // statement 
+// }
+
+// let index = 1
+// while (index <= 10) {
+//     console.log(`The value of index is ${index}`)
+//     index = index + 2
+// }
+
+//  +++++++ Iterating the array using while loop ++++++ 
+let arr = [5, 8, 2, 3, 6, 7, 8]
+let index = 0
+// while (index < arr.length) {
+//     console.log(`The value of element is ${arr[index]}`)
+//     index = index + 1
+// }
+
+// do while loop 
+// ++++++++++  Syntax ++++++++++ 
+// do {
+//     // statements 
+// } while (condition);
+
+// == special ---> do while ek bar to chalta hi chalta hai kyuki condition baad me check krta hai 
+let score = 1
+do {
+    console.log(`The value of score is ${score}`)
+    score++
+} while (score <= 10);
