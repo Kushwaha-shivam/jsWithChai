e9bda5b66d0b7d8d7e4b9f41e3a6971b7f5708a4
Shivam <kushwahashivam050@gmail.com>
2024-03-12 00:31:12 +0530

    body mass index calculator project done

---
+++ 07_dom_projects/02_bmi_generator/script.js
@@ -0,0 +1,33 @@
+const calculateBtn = document.getElementById("checkBMI")
+
+
+calculateBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+    let height = document.getElementById("height").value
+    let weight = document.getElementById("weight").value
+    let result = document.getElementById('result')
+    let bmiStatus = document.getElementById('bmi-status')
+
+    if (height === "" && height < 0 && NaN(height)) {
+        result.innerHTML = "Enter a valid value of Height"
+    } else if (weight === "" && weight < 0 && NaN(weight)) {
+        result.innerHTML = "Enter a valid value of Weight"
+    }
+    else {
+        let bmi = (weight / ((height * height) / 10000)).toFixed(1)
+        result.innerHTML = `Your BMI is ${bmi}`
+
+        if (bmi < 18.5) {
+            bmiStatus.innerHTML = "UnderWeight"
+        }
+        else if (bmi >= 18.5 && bmi <= 24.9) {
+            bmiStatus.innerHTML = "Normal"
+        }
+        else {
+            bmiStatus.innerHTML = "OverWeight"
+        }
+    }
+
+})
+
+
